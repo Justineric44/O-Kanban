5e86c498c5abdfb8e7dfacd552d7ea50f5143f6a
Justineric44 <justine.robard@hotmail.fr>
2026-03-02 13:26:21 +0100

    essai

--- api/app.js
+++ api/app.js
@@ -1,14 +1,20 @@
 import "dotenv/config";
 import express from "express";
+import cors from "cors";
+import {xss} from "express-xss-sanitizer";
 import { errorHandler } from "./middlewares/common.middleware.js";
 import listRoutes from "./routes/list.routes.js";
 import cardRoutes from "./routes/card.routes.js";
 import tagRoutes from "./routes/tag.routes.js";
 
+
+
 const PORT = process.env.PORT || 3000;
 
-const app = express();
 
+const app = express();
+app.use(cors());
+app.use(xss());
 app.use(express.json());
 
 app.use("/lists", listRoutes);
--- client/src/lib/services/list.service.js
+++ client/src/lib/services/list.service.js
@@ -1,7 +1,7 @@
-export const getLists = async () => {};
-
-export const createList = async (list) => {};
-
-export const updateList = async (list) => {};
-
-export const deleteList = async (listId) => {};
+import api from "../api";
+export const getLists = async () => api("/lists");
+export const createList = async (list) => api("/lists", "POST", list);
+export const updateList = async (list) => api(`/lists/${list.id}`, "PATCH", {
+title: list.title, position: list.position,
+});
+export const deleteList = async (listId) => api(`/lists/${listId}`, "DELETE");
