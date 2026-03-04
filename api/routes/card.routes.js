0908f16f8049cc2eb0a59edc6c1c1832fb46dcba
Justineric44 <justine.robard@hotmail.fr>
2026-03-04 14:18:40 +0100

    protection des routes en POST

--- api/routes/card.routes.js
+++ api/routes/card.routes.js
@@ -2,12 +2,13 @@ import express from "express";
 import { getAll, getById, create, update, deleteById } from '../controllers/card.controller.js';
 import { validateCardCreation, validateCardUpdate } from '../middlewares/card.middleware.js';
 import { validateId } from '../middlewares/common.middleware.js';
+import { isAllowed } from '../middlewares/auth.middleware.js';
 
 const router = express.Router();
 
 router.get('/', getAll);
 router.get('/:id', validateId, getById);
-router.post('/', validateCardCreation, create);
+router.post('/', isAllowed('admin'), validateCardCreation, create);
 router.patch('/:id', validateId, validateCardUpdate, update);
 router.delete('/:id', validateId, deleteById);
 
--- api/routes/list.routes.js
+++ api/routes/list.routes.js
@@ -2,12 +2,13 @@ import express from 'express';
 import { getAll, getById, create, update, deleteById } from '../controllers/list.controller.js';
 import { validateListCreation, validateListUpdate } from '../middlewares/list.middleware.js';
 import { validateId } from '../middlewares/common.middleware.js';
+import { isAllowed } from '../middlewares/auth.middleware.js';
 
 const router = express.Router();
 
 router.get('/', getAll);
 router.get('/:id', validateId, getById);
-router.post('/', validateListCreation, create);
+router.post('/', isAllowed('admin'), validateListCreation, create);
 router.patch('/:id', validateId, validateListUpdate, update);
 router.delete('/:id', validateId, deleteById);
 
